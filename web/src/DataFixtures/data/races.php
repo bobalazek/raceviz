21c23872294af925b0a2393cd6107dd324c8d64c
Borut Balazek <bobalazek124@gmail.com>
2021-03-11 11:37:51 +0100

    cleanup unused stuff

--- web/src/DataFixtures/data/races.php
+++ web/src/DataFixtures/data/races.php
@@ -4,8 +4,6 @@ return [
     [
         'name' => 'Formula 1 Gulf Air Bahrain Grand Prix 2021',
         'slug' => 'formula-1-gulf-air-bahrain-grand-prix-2021',
-        'location' => 'Bahrain',
-        'country_code' => 'BH',
         'laps' => 57,
         'started_at' => '2021-03-28 18:00',
         'circuit_slug' => 'bahrain',

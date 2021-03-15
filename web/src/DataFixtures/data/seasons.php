a44bca0a0e5f82d5310120ae654372cf2716dd41
Borut Balazek <bobalazek124@gmail.com>
2021-03-15 17:25:26 +0100

    Slight tweak

--- web/src/DataFixtures/data/seasons.php
+++ web/src/DataFixtures/data/seasons.php
@@ -4,11 +4,11 @@ return [
     [
         'name' => 'F1 2020',
         'slug' => 'f1-2020',
-        'series' => 'F1',
+        'series' => 'f1',
     ],
     [
         'name' => 'F1 2021',
         'slug' => 'f1-2021',
-        'series' => 'F1',
+        'series' => 'f1',
     ],
 ];
